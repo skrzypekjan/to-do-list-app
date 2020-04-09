34a3065ea1923ecff7f0ec9b5d7140a746a03c93
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-04-09 23:21:51 +0200

    environment modification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: 'AIzaSyAFyP5dHNjqzCZ7Trvoj3FhoIsxi-2EzMI',
-    authDomain: 'to-do-list-app-13463.firebaseapp.com',
-    databaseURL: 'https://to-do-list-app-13463.firebaseio.com',
-    projectId: 'to-do-list-app-13463',
-    storageBucket: 'to-do-list-app-13463.appspot.com',
-    messagingSenderId: '163374114624',
-    appId: '1:163374114624:web:94c2eb2be9a57fe5db3ca5',
-    measurementId: 'G-999BXJ2PXM'
   }
 };
 
