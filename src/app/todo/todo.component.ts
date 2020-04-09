fb678647bf78c2414d5c53d4533bde64b242ee2d
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-04-09 22:06:54 +0200

    adding services and components

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
+import {element} from 'protractor';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  providers : [TodoService]
+})
+export class TodoComponent implements OnInit {
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
+
+
+  ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+      .subscribe(item => {
+        this.toDoListArray = [];
+        // tslint:disable-next-line:no-shadowed-variable
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.toDoListArray.push(x);
+        })
+
+        // sort array isChecked false  -> true
+        this.toDoListArray.sort((a,b) => {
+          return a.isChecked - b.isChecked;
+        });
+      });
+  }
+
+
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  alterCheck($key: string, isChecked) {
+    this.toDoService.checkOrUnCheckTitle($key, !isChecked);
+  }
+
+  onDelete($key : string){
+    this.toDoService.removeTitle($key);
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyAFyP5dHNjqzCZ7Trvoj3FhoIsxi-2EzMI',
+    authDomain: 'to-do-list-app-13463.firebaseapp.com',
+    databaseURL: 'https://to-do-list-app-13463.firebaseio.com',
+    projectId: 'to-do-list-app-13463',
+    storageBucket: 'to-do-list-app-13463.appspot.com',
+    messagingSenderId: '163374114624',
+    appId: '1:163374114624:web:94c2eb2be9a57fe5db3ca5',
+    measurementId: 'G-999BXJ2PXM'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
